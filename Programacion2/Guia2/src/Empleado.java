dd2251bbe91c0701d5f84cc8f7e891ce5ec9190b
Cataldo Rodriguez Santino <107951344+SantinoCataldo@users.noreply.github.com>
2025-08-29 10:20:17 -0300

    vamos mi cariño ya no llores mas

--- Programacion2/Guia2/src/Empleado.java
+++ Programacion2/Guia2/src/Empleado.java
@@ -1,58 +1,82 @@
-public class Empleado {
+public abstract class Persona {
     private String nombre;
+    private String apellido;
+    private String gmail;
+    private String direccion;
+    private int dni;
 
-    public Empleado(String nombre) {
+    public Persona(String nombre, String apellido, String gmail, String direccion, int dni) {
         this.nombre = nombre;
+        this.apellido = apellido;
+        this.gmail = gmail;
+        this.direccion = direccion;
+        this.dni = dni;
     }
 
     public String getNombre() {
         return nombre;
     }
 
-    double calcularPago() {
-        return 0;
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getGmail() {
+        return gmail;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public abstract void mostrarDatos();
+}
+
+class Estudiante extends Persona {
+    private int cuotaMensual;
+    private int anioIngreso;
+    private String carrera;
+
+    public Estudiante(String nombre, String apellido, String gmail, String direccion, int dni, int cuotaMensual, int anioIngreso, String carrera) {
+        super(nombre, apellido, gmail, direccion, dni);
+        this.cuotaMensual = cuotaMensual;
+        this.anioIngreso = anioIngreso;
+        this.carrera = carrera;
+    }
+
+    public int getCuotaMensual() {
+        return cuotaMensual;
+    }
+
+    public void mostrarDatos() {
+        System.out.println("Nombre: " + getNombre());
+        System.out.println("Apellido: " + getApellido());
+        System.out.println("Gmail: " + getGmail());
+        System.out.println("Direccion: " + getDireccion());
+        System.out.println("DNI: " + getDni());
+        System.out.println("Cuota Mensual: " + cuotaMensual);
+        System.out.println("Año de Ingreso: " + anioIngreso);
+        System.out.println("Carrera: " + carrera);
     }
 }
 
-class EmpleadoTiempoCompleto extends Empleado {
-    double salarioMensual;
+class Staff extends Persona {
+    private String turno;
+    private double salario;
 
-    EmpleadoTiempoCompleto(String nombre, double salarioMensual) {
-        super(nombre);
-        this.salarioMensual = salarioMensual;
+
+    public Staff(String nombre, String apellido, String gmail, String direccion, int dni, String turno, double salario) {
+        super(nombre, apellido, gmail, direccion, dni);
+        this.turno = turno;
+        this.salario = salario;
     }
 
-    public double calcularPago() {
-        return salarioMensual;
-    }
-}
-
-class EmpleadoPorHoras extends Empleado {
-    double horasTrabajadas;
-    double tarifaPorHora;
-
-    EmpleadoPorHoras(String nombre, double horasTrabajadas, double tarifaPorHora) {
-        super(nombre);
-        this.horasTrabajadas = horasTrabajadas;
-        this.tarifaPorHora = tarifaPorHora;
-    }
-
-    public double calcularPago() {
-        return horasTrabajadas * tarifaPorHora;
-    }
-}
-
-class EmpleadoContratista extends Empleado {
-    double horasTrabajadas;
-    double tarifaPorProyecto;
-
-    EmpleadoContratista(String nombre, double horasTrabajadas, double tarifaPorProyecto) {
-        super(nombre);
-        this.horasTrabajadas = horasTrabajadas;
-        this.tarifaPorProyecto = tarifaPorProyecto;
-    }
-
-    public double calcularPago() {
-        return horasTrabajadas * tarifaPorProyecto;
+    public void mostrarDatos() {
+        System.out.println("Turno: " + turno);
+        System.out.println("Salario: " + salario);
     }
 }
